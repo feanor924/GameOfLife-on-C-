7b988886e3c5e12660977c94ddec36107976c808
Caner Bakar <feanor924@gmail.com>
2014-12-18 15:18:58 +0200

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -18,28 +18,33 @@ namespace Cells
 {
     class Cell 
     {
-    public:
-        Cell ();
-        Cell (int coorx,int coory);
-        Cell operator++();
-        Cell operator++(int);
-        Cell operator--(int);
-        Cell operator--();
-        void setCoorX(int x);
-        void setCoorY(int y);
-        bool operator <(const Cell &c1);
-        bool operator >(const Cell &c1);
-        bool operator >=(const Cell &c1);
-        bool operator <=(const Cell &c1);
-        bool operator ==(const Cell &c1);
-        bool operator !=(const Cell &c1);
-        int getCoorX() const { return coorX; }
-        int getCoorY() const { return coorY; }
-        friend ostream& operator << (ostream& outputStream,const Cell &c1 );
-        friend istream& operator >> (istream& inputStream ,Cell& c1);   
-    private:
+        public:
+            
+            Cell ();
+            Cell (int coorx,int coory);
+            Cell operator++();
+            Cell operator++(int);
+            Cell operator--(int);
+            Cell operator--();
+            void setCoorX(int x);
+            void setCoorY(int y);
+            bool operator <(const Cell &c1);
+            bool operator >(const Cell &c1);
+            bool operator >=(const Cell &c1);
+            bool operator <=(const Cell &c1);
+            bool operator ==(const Cell &c1);
+            bool operator !=(const Cell &c1);
+            int getCoorX() const { return coorX; }
+            int getCoorY() const { return coorY; }
+            friend ostream& operator << (ostream& outputStream,const Cell &c1 );
+            friend istream& operator >> (istream& inputStream ,Cell& c1);   
+    
+        private:
+        
         int coorX;
         int coorY;
+    
+        
     };
 }
 #endif	/* CELL_H */
