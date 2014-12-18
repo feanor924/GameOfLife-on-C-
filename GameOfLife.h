aaebc35a3e9531e80f11e285db0458cdf7405336
Caner Bakar <feanor924@gmail.com>
2014-12-18 15:18:19 +0200

    Update GameOfLife.h

--- GameOfLife.h
+++ GameOfLife.h
@@ -17,47 +17,51 @@ using namespace std;
 namespace GameLife
 {
     class GameOfLife {
-    public:
-        GameOfLife();
-        const GameOfLife& operator=(const GameOfLife &rightside);
-        GameOfLife(const GameOfLife &gameoflife);
-        void getInitials(char fileName[MAX]);
-        void printTabletoFile(char fileName1[MAX]);
-        void playGame();
-        void setStartRow(int ro);
-        void setStartColumn(int col);
-        void setCurrentX(int x);
-        void setCurrentY(int y);
-        int getStartCol() const { return StartColumn; }
-        int getStartRow() const { return StartRow; }
-        int getCurrentX() const { return currentX; }
-        int getCurrentY() const { return currentY; }
-        GameOfLife operator ++(int);
-        GameOfLife & operator ++();
-        GameOfLife operator --();
-        GameOfLife operator --(int);
-        void settings(const GameOfLife &game1,const GameOfLife &game2);
-        friend const GameOfLife operator +(const GameOfLife &game1,const GameOfLife &game2);
-        friend const GameOfLife operator -(const GameOfLife &game1,const GameOfLife &game2);
-        friend const GameOfLife operator +(GameOfLife &game1,const Cells::Cell &c1);
-        friend const GameOfLife operator -(GameOfLife &game1,const Cells::Cell &c1);
-        const GameOfLife operator +=(GameOfLife &game1);
-        friend ostream& operator <<(ostream &outputStream,GameOfLife &game1);
-        bool checkLives(int coory,int coorx);
-        ~GameOfLife();
-    private:
-        void checkColumnsRows();
-        static int cellnumber;
-        static int tempcellnumber;
-        Cells::Cell *arr1;
-        Cells::Cell *arr;
-        Cells::Cell **arr2;
-        static int count;
-        int capacity;
-        int currentX;
-        int currentY;
-        int StartRow;
-        int StartColumn;
+    
+        public:
+            
+            GameOfLife();
+            const GameOfLife& operator=(const GameOfLife &rightside);
+            GameOfLife(const GameOfLife &gameoflife);
+            void getInitials(char fileName[MAX]);
+            void printTabletoFile(char fileName1[MAX]);
+            void playGame();
+            void setStartRow(int ro);
+            void setStartColumn(int col);
+            void setCurrentX(int x);
+            void setCurrentY(int y);
+            int getStartCol() const { return StartColumn; }
+            int getStartRow() const { return StartRow; }
+            int getCurrentX() const { return currentX; }
+            int getCurrentY() const { return currentY; }
+            GameOfLife operator ++(int);
+            GameOfLife & operator ++();
+            GameOfLife operator --();
+            GameOfLife operator --(int);
+            friend const GameOfLife operator +(const GameOfLife &game1,const GameOfLife &game2);
+            friend const GameOfLife operator -(const GameOfLife &game1,const GameOfLife &game2);
+            friend const GameOfLife operator +(GameOfLife &game1,const Cells::Cell &c1);
+            friend const GameOfLife operator -(GameOfLife &game1,const Cells::Cell &c1);
+            const GameOfLife operator +=(GameOfLife &game1);
+            friend ostream& operator <<(ostream &outputStream,GameOfLife &game1);
+            ~GameOfLife();
+    
+        private:
+        
+            bool checkLives(int coory,int coorx);
+            void settings(const GameOfLife &game1,const GameOfLife &game2);
+            void checkColumnsRows();
+            static int cellnumber;
+            static int tempcellnumber;
+            Cells::Cell *arr1;
+            Cells::Cell *arr;
+            Cells::Cell **arr2;
+            static int count;
+            int capacity;
+            int currentX;
+            int currentY;
+            int StartRow;
+            int StartColumn;
     };
 
 }
